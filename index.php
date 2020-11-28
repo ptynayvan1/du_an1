ce92664c23d4c60e965bc3491c678c1f6e322875
ptynayvan1 <74031512+ptynayvan1@users.noreply.github.com>
2020-11-28 12:21:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,7 @@ session_start();
 require_once "site/index.php";
 // tao ms vua biet dung github
 // if ($_SESSION['role']==1) {
-<<<<<<< HEAD
 //     require_once "admin/index.php";
-=======
-    require_once "site/index.php";
->>>>>>> f5df35a562bb58ee8a16a3a15111e25305f27223
 // } else {
 //     require_once "site/index.php";
 // }
