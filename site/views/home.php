b66373a0f84709973c4554c7de8859d67d64ad32
tanloc8401 <74030814+tanloc8401@users.noreply.github.com>
2020-11-30 08:14:07 +0700

    ok

--- site/views/home.php
+++ site/views/home.php
@@ -77,13 +77,13 @@
                     <div class="col-lg-4 mb-4 mb-lg-0">
                         <div class="testimonial-2">
                             <blockquote class="mb-4">
-                                <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, deserunt eveniet veniam. Ipsam, nam, voluptatum"</p>
+                                <p>"Xe Rất tốt"</p>
                             </blockquote>
                             <div class="d-flex v-card align-items-center">
                                 <img src="./site/views/images/person_1.jpg" alt="Image" class="img-fluid mr-3">
                                 <div class="author-name">
-                                    <span class="d-block">Mike Fisher</span>
-                                    <span>Owner, Ford</span>
+                                    <span class="d-block">thanh hiếu</span>
+                                    <span>misubishi, Ford</span>
                                 </div>
                             </div>
                         </div>
@@ -91,13 +91,13 @@
                     <div class="col-lg-4 mb-4 mb-lg-0">
                         <div class="testimonial-2">
                             <blockquote class="mb-4">
-                                <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, deserunt eveniet veniam. Ipsam, nam, voluptatum"</p>
+                                <p>giá thuê hợp lý, xe chất lượng dịch vụ tốt </p>
                             </blockquote>
                             <div class="d-flex v-card align-items-center">
                                 <img src="./site/views/images/person_2.jpg" alt="Image" class="img-fluid mr-3">
                                 <div class="author-name">
-                                    <span class="d-block">Jean Stanley</span>
-                                    <span>Traveler</span>
+                                    <span class="d-block">Lâm Lâm</span>
+                                    <span>Du Lịch</span>
                                 </div>
                             </div>
                         </div>
@@ -105,12 +105,12 @@
                     <div class="col-lg-4 mb-4 mb-lg-0">
                         <div class="testimonial-2">
                             <blockquote class="mb-4">
-                                <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, deserunt eveniet veniam. Ipsam, nam, voluptatum"</p>
+                                <p>"good"</p>
                             </blockquote>
                             <div class="d-flex v-card align-items-center">
                                 <img src="./site/views/images/person_3.jpg" alt="Image" class="img-fluid mr-3">
                                 <div class="author-name">
-                                    <span class="d-block">Katie Rose</span>
+                                    <span class="d-block">Katie</span>
                                     <span>Customer</span>
                                 </div>
                             </div>
